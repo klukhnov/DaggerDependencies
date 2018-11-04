f55cd59b6f362f473167bdf2e396515b46a5abf7
Konstantin <klukhnov@gmail.com>
2018-11-04 18:42:00 +0100

    Initial commit

---
+++ app/src/main/java/com/example/home/testmvc/App.java
@@ -0,0 +1,19 @@
+package com.example.home.testmvc;
+
+import android.app.Application;
+
+public class App extends Application {
+    private ApplicationComponent component;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        component = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this))
+                .build();
+        }
+
+    public ApplicationComponent getComponent(){
+        return component;
+    }
+}
